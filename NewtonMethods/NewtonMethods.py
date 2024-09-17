ca09ed004eea21bae5991e1881c245d2a162e217
SalomoneCro <pedrosalomone@hotmail.com>
2024-09-17 18:34:37 -0300

    All methods to solve NonLinear Equations

--- NewtonMethods/NewtonMethods.py
+++ NewtonMethods/NewtonMethods.py
@@ -1,26 +1,98 @@
-import numpy
-import autograd.numpy 
+import autograd.numpy as np
 from autograd import jacobian
 
 def NewtonMethod(F: callable, 
-                 x0: autograd.numpy.ndarray, 
+                 x0: np.ndarray, 
                  tol: float, 
                  maxit: int
-) -> autograd.numpy.ndarray:
+) -> np.ndarray:
+    
     jac = jacobian(F)
-
-    cond = numpy.linalg.norm(F(x0)) <= tol
-    s = numpy.zeros(len(x0))
+    xi = x0
+    Fe = F(xi)
+    cond = np.linalg.norm(F(x0)) > tol
     i = 0
 
-    while (not cond) and i < maxit:
-        xi = x0 + s
-
-        s = numpy.linalg.solve(a = jac(xi),
-                               b = -F(xi))
-
+    while cond and i < maxit:
+          
+        s = np.linalg.solve(a = jac(xi),
+                               b = -Fe)
+        xi = xi + s
         i += 1
-        cond = numpy.linalg.norm(F(x0)) <= tol
+        Fe = F(xi)
+        cond = np.linalg.norm(Fe) > tol
     
     return xi
 
+def StationaryNewtonMethod(F: callable, 
+                           x0: np.ndarray, 
+                           tol: float, 
+                           maxit: int
+) -> np.ndarray:
+    
+    J = jacobian(F)(x0)
+    xi = x0
+    Fe = F(xi)
+    cond = np.linalg.norm(Fe) > tol
+    i = 0
+
+    while cond and i < maxit:
+          
+        s = np.linalg.solve(a = J,
+                            b = -Fe)
+        xi = xi + s
+        i += 1
+        Fe = F(xi)
+        cond = np.linalg.norm(Fe) > tol
+    
+    return xi
+
+def mStationaryNewtonMethod(F: callable, 
+                            x0: np.ndarray, 
+                            tol: float, 
+                            maxit: int,
+                            m: int
+) -> np.ndarray:
+    
+    jac = jacobian(F)
+    xi = x0
+    Fe = F(xi)
+    cond = np.linalg.norm(Fe) > tol
+    i = 0
+
+    while cond and i < maxit:
+        if i % m == 0:
+            J = jac(xi)
+        s = np.linalg.solve(a = J,
+                            b = -Fe)
+        xi = xi + s
+        i += 1
+        Fe = F(xi)
+        cond = np.linalg.norm(Fe) > tol
+    
+    return xi
+
+def BroydenMethod(F: callable, 
+                 x0: np.ndarray, 
+                 tol: float, 
+                 maxit: int
+) -> np.ndarray:
+    
+    J = jacobian(F)(x0)
+    xi = x0
+    Fe = F(xi)
+    cond = np.linalg.norm(Fe) > tol
+    i = 0
+
+    while cond and i < maxit:
+
+        s = np.linalg.solve(a = J,
+                            b = -Fe)
+        yi = F(xi + s) - Fe
+        xi = xi + s
+        Fe = F(xi)
+        i += 1
+        cond = np.linalg.norm(Fe) > tol
+        J += np.inner((yi - J @ s), s) / np.inner(s,s)
+    
+    return xi
